831085bbeadc789254a615b9e8bf8c38e1047041
来诺 <winfield301@gmail.com>
2017-05-29 21:47:07 +0800

    Add redux integration with apollo

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import ApolloClient, { createNetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
@@ -11,8 +12,18 @@ const client = new ApolloClient({
   dataIdFromObject: o => o.id,
 })
 
+const store = createStore(
+  combineReducers({
+    apollo: client.reducer(),
+  }),
+  {},
+  compose(
+    applyMiddleware(client.middleware()),
+  ),
+)
+
 const App = () => (
-  <ApolloProvider client={client}>
+  <ApolloProvider store={store} client={client}>
     <Router>
       <div>
         <Route exact path='/' component={ArticlesListPage} />
@@ -20,7 +31,6 @@ const App = () => (
       </div>
     </Router>
   </ApolloProvider>
-
 )
 
 export default App
