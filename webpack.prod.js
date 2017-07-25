fd9d0a3d471ab27dcb98ca1dbe20ab61d570ef1a
Jeff Lai <winfield301@gmail.com>
2017-07-25 11:06:44 +0800

    Use webpack definePlugin to make use of react production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -31,15 +31,15 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+
     new webpack.optimize.UglifyJsPlugin({
       beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
+      sourceMap: true,
       comments: false,
     }),
   ]
